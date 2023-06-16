c7d53c472f27551d32eeb1c5781f7133efe20dd1
김명식 <rlaaudtlr233@naver.com>
2023-06-16 13:35:17 +0900

    [feat] todo form에 todo를 추가하는 함수 추가

--- src/components/forms/TodoAddForm.tsx
+++ src/components/forms/TodoAddForm.tsx
@@ -1,11 +1,23 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Todo } from "../lists/TodoList";
 
-export default function TodoAddForm() {
+type Props = {
+  handleAddTodo: (todo: Todo) => void;
+};
+
+export default function TodoAddForm({ handleAddTodo }: Props) {
   const [addText, setAddText] = useState("");
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(addText.replaceAll(" ", ""));
+    // console.log(addText.replaceAll(" ", ""));
+    handleAddTodo({
+      id: "고유한값",
+      text: addText.replaceAll(" ", ""),
+      status: "active",
+    });
+
+    setAddText(() => "");
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
--- src/components/lists/TodoList.tsx
+++ src/components/lists/TodoList.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import TodoAddForm from "../forms/TodoAddForm";
 
+export type Todo = {
+  id: string;
+  text: string;
+  status: string;
+};
+
 export default function TodoList() {
-  const [todos, setTodos] = useState([
-    { id: 123, text: "장보기", status: "active" },
-    { id: 124, text: "공부하기", status: "active" },
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: "123", text: "장보기", status: "active" },
+    { id: "124", text: "공부하기", status: "active" },
   ]);
+
+  const handleAddTodo = (todo: Todo) => {
+    setTodos((prev) => [...prev, todo]);
+  };
 
   return (
     <section>
@@ -14,7 +24,7 @@ export default function TodoList() {
           <li key={id}>{text}</li>
         ))}
       </ul>
-      <TodoAddForm />
+      <TodoAddForm handleAddTodo={handleAddTodo} />
     </section>
   );
 }
