830b7ca8492ec9291cd8bbc3efe262fbac6632ec
김명식 <rlaaudtlr233@naver.com>
2023-06-18 12:04:23 +0900

    [design] filter header 스타일링

--- src/components/headers/FilterHeader.tsx
+++ src/components/headers/FilterHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Styles from "./FilterHeader.module.css";
 
 type Props = {
   onUpdateFilter: (filterVal: "all" | "completed" | "active") => void;
@@ -11,10 +12,25 @@ export default function FilterHeader({ onUpdateFilter }: Props) {
   };
 
   return (
-    <header>
-      <button onClick={() => handleUpdate("all")}>all</button>
-      <button onClick={() => handleUpdate("active")}>active</button>
-      <button onClick={() => handleUpdate("completed")}>completed</button>
+    <header className={Styles["header"]}>
+      <button
+        onClick={() => handleUpdate("all")}
+        className={Styles["filter-btn"]}
+      >
+        all
+      </button>
+      <button
+        onClick={() => handleUpdate("active")}
+        className={Styles["filter-btn"]}
+      >
+        active
+      </button>
+      <button
+        onClick={() => handleUpdate("completed")}
+        className={Styles["filter-btn"]}
+      >
+        completed
+      </button>
     </header>
   );
 }
