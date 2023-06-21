375b9bc510e481a47133088415fc74159c399050
김명식 <rlaaudtlr233@naver.com>
2023-06-21 16:35:37 +0900

    [feat] 다크모드 프로바이더 마운트시 사용자가 설정한 다크모드 라이트모드 값과 이전에 애플리케이션에서 설정한 값을 가져와 적용시키는 기능 구현, 로컬스토리지 이용

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 const DarkModeContext = createContext({
   darkMode: false,
@@ -11,6 +17,16 @@ export function DarkModeProvider({ children }: { children: ReactNode }) {
     setDarkMode((mode) => !mode);
     updateDarkMode(!darkMode);
   };
+  // 다크모드 컨텍스트 프로바이더 초기 마운트 시
+  // 사용자의 브라우저 다크모드, 라이트모드 설정값을 가져와 셋팅
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
 
   return (
     <DarkModeContext.Provider
@@ -20,12 +36,15 @@ export function DarkModeProvider({ children }: { children: ReactNode }) {
     </DarkModeContext.Provider>
   );
 }
-
+// 컨텍스트에서 컨트롤하는 다크모드 상태에 따라
+// html 문서에 클래스를 추가 및 제거, 로컬스토리지에 다크모드 상태를 저장("dark", "light")
 function updateDarkMode(darkMode: boolean) {
   if (darkMode) {
     document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
   }
 }
 
