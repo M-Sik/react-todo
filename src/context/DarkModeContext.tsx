68e68d3172f7a76702ceead9cf61633739aa6b40
김명식 <rlaaudtlr233@naver.com>
2023-06-21 16:08:55 +0900

    [feat] 다크모드, 라이트모드를사용하기위해 DarkModeContext.tsx 파일 작성

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+
+const DarkModeContext = createContext({});
+
+export function DarkModeProvider({ children }: { children: ReactNode }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => setDarkMode((mode) => !mode);
+
+  return (
+    <DarkModeContext.Provider
+      value={{ darkMode: darkMode, toggleDarkMode: toggleDarkMode }}
+    >
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export const useDarkMode = () => useContext(DarkModeContext);
